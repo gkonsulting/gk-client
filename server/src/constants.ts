9072d45e25c9e7343441f239b84750709eff949b
ianevangelista <ianevangelista1999@gmail.com>
2020-11-12 14:48:07 +0100

    inital commit

--- server/src/constants.ts
+++
@@ -1,3 +0,0 @@
-export const __prod__ = process.env.NODE_ENV === "production";
-export const COOKIE_NAME = "qid";
-export const RESET_PASSWORD_PREFIX = "reset-password";
--- server/src/types.ts
+++
@@ -1,7 +0,0 @@
-import { Request, Response } from "express";
-import { Redis } from "ioredis";
-export type MyContext = {
-    res: Response;
-    req: Request & { session?: Express.Session };
-    redis: Redis;
-};
